dd29b6c729fccb7f40d5c92abe40025de7edfc26
RobisonTorres <robison.torres21@gmail.com>
2025-05-03 17:25:17 -0300

    first commit.

---
+++ src/test/java/com/unifecaf/Library_Manager/LibraryManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unifecaf.Library_Manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
